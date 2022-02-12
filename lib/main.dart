c561c2d7bd8393c050eee1840d4b378d4829dda5
shashwat28 <shashwat3770@gmail.com>
2022-02-12 15:15:07 +0530

    form added

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,18 @@
 import 'package:flutter/material.dart';
+import './form_screen.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:HomePage(),
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-  ));
-}
+void main() => runApp(MyApp());
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("DUMB TRACKER"),
+    return MaterialApp(
+      title: 'Coding with Curry',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        color:Colors.indigo[900],
-        width: 500,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-          padding: const EdgeInsets.all(8),
-          width:350,
-          height: 200,
-          color: Colors.blue[900],
-        ),
-          ],
-        ),
-      ),
+      home: FormScreen(),
     );
   }
 }
